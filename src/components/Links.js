7819b35184d9640c17857d2a21fab96a1ce4168d
foghill <ben@thefoghill.com>
2022-04-27 15:18:18 -0400

    commit tree f31c7ccad4d098cf3e89ecacf710fb38d52d4edb

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Links() {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href="{props.github}">LinkedIn</a>
+      <a href="{props.linkedin}">GitHub</a>
+    </>
+  );
+}
+
+export default Links;
